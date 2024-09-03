3ebeb391b4d69fc625b7860a2bab619bd54118c8
quibolharvey <harveyquibol>
2024-09-03 09:42:28 +0800

    harvey

---
+++ resources/views/pages/profile.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/bootsrap.css')}}">
+    <title>Document</title>
+</head>
+<body>
+    <h1>PROFILE</h1>
+    <a href="{{url('/')}}">HOME</a>||
+    <a href="http://sulasok.org">EXIT</a> ||
+    <a href="http://facebook.org">EXIT</a>
+</body>
+</html>
